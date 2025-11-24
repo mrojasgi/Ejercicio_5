1973abca595c2b6148c773e306c15734386b14d9
mrojasgi <103543000+mrojasgi@users.noreply.github.com>
2025-11-24 15:44:14 -0500

    Add main method to launch interfaz window

--- principal.java
+++ principal.java
@@ -1,1 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
 
+package com.mycompany.poo_t5;
+
+
+public class POO_T5 {
+
+    public static void main(String[] args) {
+        interfaz ventana = new interfaz();
+        ventana.setVisible(true);
+    }
+}
